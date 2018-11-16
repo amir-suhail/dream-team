b0d2b376d9df17dc9b0280bd77e261cb2f0fd69e
Amir Suhail V <asuhail@altimetrik.com>
2018-11-16 17:11:16 +0530

    indentation fixes

--- controller/Master.controller.js
+++ controller/Master.controller.js
@@ -1,14 +1,12 @@
 sap.ui.define([
-               "sap/ui/core/mvc/Controller"
-               ], function (Controller, JSONModel) {
-  "use restrict";
-  return Controller.extend("sap.ui.demo.wt.controller.Master",{
-  onInit: function () {
-  },
-  onPress: function(oEvent)
-  {
-  var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-  oRouter.navTo("second");
-  }
-  });
+    "sap/ui/core/mvc/Controller"
+], function (Controller, JSONModel) {
+    "use restrict";
+    return Controller.extend("sap.ui.demo.wt.controller.Master",{
+        onInit: function () {},
+        onPress: function(oEvent) {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("second");
+		}
+    });
 });
--- controller/Slave.controller.js
+++ controller/Slave.controller.js
@@ -1,21 +1,19 @@
 sap.ui.define([
-               "sap/ui/core/mvc/Controller",
-               "sap/ui/core/routing/History"
-               ], function (Controller, History) {
-  "use restrict";
-  return Controller.extend("sap.ui.demo.wt.controller.Slave",{
-  onInit: function () {
-  },
-  onBack: function()
-  {
-  var oHistory = History.getInstance();
-  var sPreviousHash = oHistory.getPreviousHash();
-  if (sPreviousHash !== undefined) {
-  window.history.go(-1);
-  } else {
-  var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-  oRouter.navTo("overview", true);
-  }
-  }
-  });
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
+], function (Controller, History) {
+    "use restrict";
+    return Controller.extend("sap.ui.demo.wt.controller.Slave",{
+        onInit: function () {},
+        onBack: function() {
+            var oHistory = History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("master", true);
+            }
+        }
+    });
 });
